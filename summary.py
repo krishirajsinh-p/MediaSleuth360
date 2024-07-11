86abea891f11d27c7fe6322ad6a8215d30a4ad73
krishirajsinh-p <krishirajpuwar@gmail.com>
2024-07-12 00:47:43 +0200

    improved prompts and making use of file.type

--- summary.py
+++ summary.py
@@ -3,26 +3,28 @@ from response import generate_response
 from transcribe import generate_transcript
 
 @st.cache_data()
-def generate_summary(file) -> str:
-    transcript = generate_transcript(file)
-    
-    system_prompt = """You are a helpful assistant you should be able \
-to generate a summary from the media transcript. The summary \
-should have important chapters or topics disscussed in \
-the media along with the timestamps.
+def generate_summary(file, filetype) -> str:
+    transcript = generate_transcript(file, filetype)
 
-you should follow the format give below:
-start_time - end_time : Chapter 1 summary.
-start_time - end_time : Chapter 2 summary.
-start_time - end_time : Chapter 3 summary.
+    system_prompt = f"""You are an expert media analyst. Your task is \
+to create a structured summary of the given {filetype} transcript. The \
+summary should include key chapters or topics discussed, each with \
+corresponding timestamps. Follow the exact format provided below:
+
+<format-start>
+start_time - end_time: Topic 1 summary.
+start_time - end_time: Topic 2 summary.
+start_time - end_time: Topic 3 summary.
 ...
+<format-end>
 
-
-Overall summary of the media: <summary of the media>.
-
-Note: strictly follow the format and do not include any other information.\
+Note: Ensure that the summary is concise and focuses on the main points. \
+Strictly adhere to the format and do not include any additional information \
+or commentary. Do not include the start and end tags in the summary itself.
 """
 
-    user_prompt = f"generate the summary from the media transcript:\n\n{transcript}"
+    user_prompt = f"""Generate a structured summary from the {filetype} \
+transcript provided below, identifying key chapters or topics discussed \
+along with their timestamps:\n\n{transcript}"""
 
     return generate_response(system_prompt, user_prompt)
