234cd755decc668f5ea1f9997e240ac0224f743d
krishirajsinh-p <krishirajpuwar@gmail.com>
2024-07-11 23:09:09 +0200

    adding filetype info in transcript.

--- transcribe.py
+++ transcribe.py
@@ -3,9 +3,9 @@ from raw_data import generate_raw
 import time
 
 @st.cache_data()
-def generate_transcript(file) -> str:
-    transcript = ""
-    for line in generate_raw(file).segments:
+def generate_transcript(file, filetype) -> str:
+    transcript = f"filetype: {filetype}\n\n"
+    for line in generate_raw(file, filetype).segments:
         start_time = time.strftime('%H:%M:%S', time.gmtime(line['start']))
         end_time = time.strftime('%H:%M:%S', time.gmtime(line['end']))
         transcript += f"{start_time} - {end_time} : {line['text']}\n"
