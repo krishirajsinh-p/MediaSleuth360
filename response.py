c99cdcfe11ba2a863e5867f845d4faf9724eda78
krishirajsinh-p <krishirajpuwar@gmail.com>
2024-07-14 17:00:48 +0200

    mix of models

--- response.py
+++ response.py
@@ -1,7 +1,17 @@
 import streamlit as st
 from groq import Groq
+import google.generativeai as genai
 
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
+
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+model = genai.GenerativeModel(
+    model_name="models/gemini-1.5-flash",
+    generation_config=genai.GenerationConfig(
+        temperature=0.1,
+        top_p=0.95
+    )
+)
 
 def generate_response(system: str, prompt: str) -> str:
     try:
@@ -22,28 +32,8 @@ def generate_response(system: str, prompt: str) -> str:
         )
         return response.choices[0].message.content
     except Exception as e:
-        return f"An error occurred: {e}"
-
-# import google.generativeai as genai
-# 
-# genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
-# model = genai.GenerativeModel(
-#     model_name="models/gemini-1.5-flash",
-#     generation_config=genai.GenerationConfig(temperature=0.1, top_p=0.95)
-# )
-
-# def generate_response(prompt: str, system: str) -> str:
-#     try:
-#         response = model.generate_content([system, prompt])
-#         print(response)     #remove this line
-#         if response.candidates and response.candidates[0].safety_ratings:
-#             # Check if the content is blocked due to safety concerns
-#             if any(rating.blocked for rating in response.candidates[0].safety_ratings):
-#                 return "Content blocked due to safety concerns."
-#         # Check if there are valid parts in the response
-#         if response.candidates and response.candidates[0].content:
-#             return response.text
-#         else:
-#             return "No valid response generated."
-#     except Exception as e:
-#         return f"An error occurred: {e}"
+        try:
+            response = model.generate_content([system, prompt])
+            return response.text
+        except Exception as e:
+            return f"An error occurred: {e}"
