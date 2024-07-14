747b38e0842b26dc6d1745b36585d8f9d2cdc037
krishirajsinh-p <krishirajpuwar@gmail.com>
2024-07-14 16:00:10 +0200

    error handling

--- response.py
+++ response.py
@@ -4,22 +4,22 @@ from groq import Groq
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
 def generate_response(system: str, prompt: str) -> str:
-    response = client.chat.completions.create(
-        messages=[
-            {
-                "role": "system",
-                "content": system
-            },
-            {
-                "role": "user", 
-                "content": prompt
-            }
-        ],
-        model="llama3-8b-8192",
-        temperature=0.1,
-        top_p=0.95,
-    )
     try:
+        response = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "system",
+                    "content": system
+                },
+                {
+                    "role": "user", 
+                    "content": prompt
+                }
+            ],
+            model="llama3-8b-8192",
+            temperature=0.1,
+            top_p=0.95,
+        )
         return response.choices[0].message.content
     except Exception as e:
         return f"An error occurred: {e}"
